c62262c617a5fd7ef728cbf6960ef7d1d3323e76
Phong V. Cao <phongvcao@phongvcao.com>
2020-01-27 01:25:54 -0800

    Edited setup

--- home/pdbrc.py
+++ home/pdbrc.py
@@ -6,6 +6,8 @@ class Config( pdb.DefaultConfig ):
     line_number_color = '32;01'
     filename_color = '33'
     current_line_color = '32'
+    pygments_formatter_class = "pygments.formatters.TerminalTrueColorFormatter"
+    pygments_formatter_kwargs = {"bg": "dark", "style": "solarizeddark"}
 
     def setup( self, pdb ):
         pass
