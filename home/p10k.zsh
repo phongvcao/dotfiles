c236abfd8079685164105c6eda57dac5c6896640
Phong V. Cao <phongvcao@phongvcao.com>
2020-07-17 05:20:09 -0700

    Change color of dir

--- home/p10k.zsh
+++ home/p10k.zsh
@@ -212,7 +212,7 @@
   # Current directory background color.
   typeset -g POWERLEVEL9K_DIR_BACKGROUND=0
   # Default current directory foreground color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=10
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=14
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
