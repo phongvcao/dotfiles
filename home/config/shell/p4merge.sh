59efa2d0302e63ba65b45b52ad6612d57a234414
Phong V. Cao <phongvcao@phongvcao.com>
2020-08-29 14:59:37 -0700

    Ready for merge

--- home/config/shell/p4merge.sh
+++ home/config/shell/p4merge.sh
@@ -5,4 +5,4 @@
 # MERGED=$4
 # command nvim -d $1 $2 $3 $4
 # command nvim -f -c "4wincmd w | normal! 'gg100000dd' | diffoff" -d "$BASE" "$REMOTE" "$LOCAL" "$MERGED"
-command nvim -f -c "1wincmd w" -c "diffoff" -c "4wincmd w | normal! gg100000dd" -d "$1" "$2" "$3" "$4" -c 'diffoff' -c '3wincmd w'
+command nvim -f -c "1wincmd w" -c "diffoff" -c "4wincmd w | normal! gg100000dd" -d "$1" "$2" "$3" "$4" -c 'diffoff' -c "1wincmd w" -c "set readonly" -c "2wincmd w" -c "set readonly" -c '3wincmd w' -c "set readonly"
