88b555f2833056ef2b21c96fbb6d825fc285d498
Phong V. Cao <phongvcao@phongvcao.com>
2023-10-11 07:39:50 -0500

    Added plugins for zsh

--- home/config/antigen/antigenrc.zsh
+++ home/config/antigen/antigenrc.zsh
@@ -6,7 +6,8 @@ else
 fi
 
 # Load the oh-my-zsh's library.
-antigen use oh-my-zsh
+# antigen use oh-my-zsh
+antigen use ohmyzsh/ohmyzsh
 
 # # Bundles from the default repo (robbyrussell's oh-my-zsh).
 # antigen bundle git
@@ -14,13 +15,16 @@ antigen use oh-my-zsh
 # antigen bundle pip
 # antigen bundle lein
 antigen bundle command-not-found
-antigen bundle command-not-found
+antigen bundle colored-man-pages
 
 # Syntax highlighting bundle.
-antigen bundle zsh-users/zsh-syntax-highlighting
+# antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle agkozak/zsh-z
+antigen bundle zdharma-continuum/fast-syntax-highlighting
+antigen bundle zdharma-continuum/history-search-multi-word --branch=main
 
 # # Load the theme.
 # antigen theme robbyrussell
