37798430381e72d9736ca214fd7a5d1fd2f910cf
Phong V. Cao <phongvcao@phongvcao.com>
2023-10-11 11:37:31 -0500

    Added plugins

--- home/config/antigen/antigenrc.zsh
+++ home/config/antigen/antigenrc.zsh
@@ -25,9 +25,20 @@ antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle agkozak/zsh-z
 antigen bundle zdharma-continuum/fast-syntax-highlighting
 antigen bundle zdharma-continuum/history-search-multi-word --branch=main
+antigen bundle joel-porquet/zsh-dircolors-solarized.git
+
+# NVM bundle
+export NVM_LAZY_LOAD=true
+antigen bundle lukechilds/zsh-nvm
+antigen bundle Sparragus/zsh-auto-nvm-use
 
 # # Load the theme.
-# antigen theme robbyrussell
+antigen theme romkatv/powerlevel10k
 
 # Tell Antigen that you're done.
 antigen apply
+
+# zsh-dircolors-solarized settings
+if [[ -f $HOME/.antigen/bundles/joel-porquet/zsh-dircolors-solarized/dircolors-solarized/dircolors.ansi-dark ]]; then
+    source $HOME/.antigen/bundles/joel-porquet/zsh-dircolors-solarized/zsh-dircolors-solarized.zsh
+fi
